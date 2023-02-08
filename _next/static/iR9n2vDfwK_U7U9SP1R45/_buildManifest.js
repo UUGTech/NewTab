78e81e9a29342b60b22890c16840127b02444b70
UUGTech <UUGTech@users.noreply.github.com>
2023-02-08 11:27:49 +0000

    deploy: 591fba0c320bf3f55794bd06f00dab108270decd

---
+++ _next/static/iR9n2vDfwK_U7U9SP1R45/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3393d4ae0a046543.css","static/chunks/pages/index-6d523596e05f18af.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
