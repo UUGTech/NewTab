74cd14e318a629fd6c196f2ffb3141046b7a5ab0
UUGTech <UUGTech@users.noreply.github.com>
2023-02-08 11:08:32 +0000

    deploy: 2504fc23de882ce92583563f56051b48aef28b64

--- _next/static/v5sSgeTzTdRdbeQeOL4Dw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8bcd170f2695b8fe.css","static/chunks/pages/index-304b76985ed3c5f7.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
